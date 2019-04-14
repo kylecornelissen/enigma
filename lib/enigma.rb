55472e1439fccdccb5580986dc5e2d13666f375e
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-04-14 02:16:25 -0600

    Add key_randomizer method to Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,15 @@
+require 'date'
+
 class Enigma
   def encrypt(message, key, date)
+    key = key_randomizer
     encrypt_hash = {:encryption => message, :key => key, :date => date}
   end
+
+  def key_randomizer
+    "%05d" % rand(10**5)
+  end
+
+  # get date and square it
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,12 +10,17 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt_returns_hash_with_encrypted_message_and_key_and_date
+    skip
     expected = {
-      encryption: "keder ohulw",
+      encryption: "hello world",
       key: "02715",
       date: "040895"
     }
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_key_randomizer
+    assert 5, @enigma.key_randomizer.size
   end
 
 end
