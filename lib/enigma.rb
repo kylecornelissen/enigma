e22e47676bbbdd946b1689e5f4ebe37850cfa26c
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-04-14 01:42:04 -0600

    Add encrypt method to enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,6 @@
 class Enigma
-
+  def encrypt(message, key, date)
+    encrypt_hash = {:encryption => message, :key => key, :date => date}
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,10 +1,21 @@
 require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
 
   def test_enigma_class_exists
-    @enigma = Enigma.new
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_encrypt_returns_hash_with_encrypted_message_and_key_and_date
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 
 end
