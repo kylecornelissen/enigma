162125e95379a3682b9b73052730794789786b06
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-04-14 16:32:22 -0600

    Add split_last_four method to Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -30,4 +30,12 @@ class Enigma
     square_date[-4..-1]
   end
 
+  def split_last_four
+    alfa_offset = square_date_last_four[0]
+    bravo_offset = square_date_last_four[1]
+    charlie_offset = square_date_last_four[2]
+    delta_offset = square_date_last_four[3]
+    offsets = [alfa_offset, bravo_offset, charlie_offset, delta_offset]
+  end
+
 end
