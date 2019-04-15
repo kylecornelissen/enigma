d0b022537340cbf642baa740a5a640509211e9ee
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-04-15 16:36:48 -0600

    Moved key_shifter method to Shifter class and created initialize method with KeyGen and OffSet objects.

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,3 +1,18 @@
 class Shifter
+  attr_reader :key_gen,
+              :offset,
+              :characters
+  def initialize
+    @key_gen = KeyGen.new
+    @offset = OffSet.new
+    @characters = ("a".."z").to_a << " "
+  end
 
+  def key_shifter(split_keys = @key_gen.split_keys, split_last_four = @offset.split_last_four)
+    alfa_shift = split_keys[0] + split_last_four[0]
+    bravo_shift = split_keys[1] + split_last_four[1]
+    charlie_shift = split_keys[2] + split_last_four[2]
+    delta_shift = split_keys[3] + split_last_four[3]
+    shifter_keys = [alfa_shift, bravo_shift, charlie_shift, delta_shift]
+  end
 end
--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -9,4 +9,8 @@ class ShifterTest < Minitest::Test
     assert_instance_of Shifter, @shifter
   end
 
+  def test_key_shifter_adds_split_last_four_and_split_keys
+    assert_equal 4, @shifter.key_shifter(@shifter.key_gen.split_keys, @shifter.offset.split_last_four).length
+  end
+
 end
