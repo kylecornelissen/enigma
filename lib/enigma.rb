95aec4aea4bd141795441444b93a5400a553241a
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-04-15 17:01:04 -0600

    Created DateToday module included in the Enigma and Offset classes.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,10 @@
+require_relative 'date_today'
+
 class Enigma
+  include DateToday
+
   def encrypt(message, key = KeyGen.new, date = current_date)
     encrypt_hash = {:encryption => message, :key => key, :date => date}
-  end
-
-  def current_date
-    Time.now.strftime("%d%m%y")
   end
 
 end
