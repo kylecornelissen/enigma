dd6bfa3e1f6cf70e975141d539ff41392da2a32f
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-04-15 21:43:12 -0600

    Added initialize method with optional random_key argument for KeyGen

--- test/key_gen_test.rb
+++ test/key_gen_test.rb
@@ -2,20 +2,25 @@ require './test/test_helper'
 
 class KeyGenTest < Minitest::Test
   def setup
-    @key_gen = KeyGen.new
+    @key_gen = KeyGen.new("02715")
   end
 
   def test_keygen_class_exists
     assert_instance_of KeyGen, @key_gen
   end
 
-  def test_random_key_initializes_five_character_string
-    assert_equal 5, @key_gen.random_key.size
-    assert_instance_of String, @key_gen.random_key
+  def test_initializes_with_keygen_argument
+    assert_equal "02715", @key_gen.random_key
+  end
+
+  def test_random_key_generates_random_key_when_no_argument_given
+    key_gen2 = KeyGen.new
+    assert_equal 5, key_gen2.random_key.size
+    assert_instance_of String, key_gen2.random_key
   end
 
   def test_split_key_splits_random_key_into_four_keys
-    assert_equal 4, @key_gen.split_keys.count
+    assert_equal ["02", "27", "71", "15"], @key_gen.split_keys
   end
 
 end
